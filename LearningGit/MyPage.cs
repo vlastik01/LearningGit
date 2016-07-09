8d532143de0903f83ee4d62e3fd3e88db830e09c
vlastik01 <vlastimil.adamovsky@gmail.com>
2016-07-09 17:42:06 -0400

    hello

---
+++ LearningGit/MyPage.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace LearningGit
+{
+	public class MyPage : ContentPage
+	{
+		public MyPage()
+		{
+			Content = new StackLayout
+			{
+				Children = {
+					new Label { Text = "Hello ContentPage" }
+				}
+			};
+		}
+	}
+}
+
+
