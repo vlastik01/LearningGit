dade0c49b81d93d114cb0ff579945d50bfae1ed4
vlastik01 <vlastimil.adamovsky@gmail.com>
2016-07-12 17:44:25 -0400

    eeeeeee

--- LearningGit/Main.cs
+++ LearningGit/Main.cs
@@ -4,7 +4,7 @@ namespace LearningGit
 {
 	public class Application
 	{
-		// This is the main entry point of the application.
+		// This is the main entry point of the   application.
 		   static void Main(string[] args)
 		{
 			// if you want to use a different Application Delegate class from "AppDelegate"
